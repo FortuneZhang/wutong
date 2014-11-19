fd990f772948cbecd6c0e104405b886340b3d21d
FortuneZhang <zhangfortune527@gmail.com>
2014-11-19 13:23:56 +0800

    fortune- add config.cfg

--- Config/db.py
+++ Config/db.py
@@ -1,13 +1,19 @@
 __author__ = 'Administrator'
 
-import pyodbc
+import pyodbc, os
+
+
 class SQLServerDriver():
     def __init__(self):
         self.connection = ''
+        self.config = Config()
 
 
     def getConnection(self):
-        self.connection =  pyodbc.connect('DRIVER={SQL Server};SERVER=2012-20140806KE\SQLEXPRESS;DATABASE=test3;UID=sa;PWD=123456',charset="UTF-8")
+        db = self.config.getDb();
+        conn_str = 'DRIVER={SQL Server};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s' % (
+            db['SERVER_NAME'], db['DATABASE'], db['UID'], db['PWD'])
+        self.connection = pyodbc.connect(conn_str, charset="UTF-8")
         return self.connection
 
 
@@ -16,4 +22,42 @@ class SQLServerDriver():
 
 
 class Config():
-    pass 
+    DB = {
+        'SERVER_NAME': '',
+        'DATABASE': '',
+        'UID': '',
+        'PWD': ''
+    }
+
+    syncInterval = 10
+
+    def __init__(self):
+        pass
+
+    def _read_config_file(self):
+        file = os.path.join(os.getcwd() + '\\' + 'config.cfg')
+        f = open(file)
+        for line in f.readlines():
+            if line == '' or line.startswith('//'):
+                pass
+            else:
+                c = line.split(' ')
+                if c[0] in self.__class__.DB.keys():
+                    self.__class__.DB[c[0]] = c[1].strip()
+                elif c[0] == 'syncInterval':
+                    try:
+                        self.__class__.syncInterval = int(c[1].strip())
+                    except:
+                        print 'cant load syncInterval'
+
+        f.close()
+
+    def getSyncInterval(self):
+        if self.syncInterval == 10:
+            self._read_config_file()
+        return self.__class__.syncInterval
+
+    def getDb(self):
+        if '' in self.DB.values():
+            self._read_config_file()
+        return self.__class__.DB
--- DataQueue/requestCheInfo.py
+++ DataQueue/requestCheInfo.py
@@ -1,10 +1,12 @@
 import threading
 from Queue import Queue
+from config.db import SQLServerDriver
 
 
 class RequestCheInfo(threading.Thread):
     def __init__(self):
         pass
+
 
     def run(self):
         pass
